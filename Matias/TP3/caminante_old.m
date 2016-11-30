5867954f36da536e84cb704f3b6d41042ead7bb8
Matias Vazquez <matias@matias-debian>
2016-11-30 11:14:33 -0300

    terminado el tp3

--- Matias/TP3/caminante_old.m
+++ Matias/TP3/caminante_old.m
@@ -5,11 +5,14 @@
 % Hay que unir con lineas los pesos de las neuronas adyacentes
 clear all;
 clc;
-M = 10; % cantidad de puntos
+M = 50; % cantidad de puntos
 N = M*2; % numero de neuronas
 aprendizaje = 0.01;
-sigma = 5;
-limite = 1e-3;
+sigma = 0.5;
+k = sigma/10;
+limite = 5e-3;
+
+iteraciones = 100;
 
 puntos = rand(M,2)*2 -1;
 
@@ -26,40 +29,55 @@ plot(puntos(:,1), puntos(:,2), '.r');
 title('w inicial');
 saveas( gcf, fullfile('ej3','0'), 'jpg' );
 
-
 iteracion = 1;
 while sigma > limite
+   
+    for n = 1:iteraciones
     
-    for k=1:size(puntos,1)
-        
-        min = Inf;
-        for j = 1:size(w,2)
-            aux = norm(w(j,:) - puntos(k,:));
-            if aux < min
-                min = aux;
-                pos = j
-            end
-        end 
-        
-        % en base al minimo modifico la red
         for i = 1:size(w,1)
-            vecindad = exp(-(norm(i-pos)^2)/(2*sigma^2));
-            incremento = aprendizaje * vecindad * (puntos(k,:)-w(i,:));
+
+            incremento = 0;
+
+            for mu = 1:size(puntos,1);
+
+                divisor = 0;
+                for j = 1:size(w,1)
+                    aux = exp(-( norm(puntos(mu,:) - w(j,:)) )^2/(2*sigma^2));
+                    divisor = divisor + aux;
+                end
+
+                vecindad = exp(-(( norm(puntos(mu,:) - w(i,:)) )^2)/(2*sigma^2)) / divisor;
+                incremento = incremento + vecindad * (puntos(mu,:) - w(i,:));
+
+            end
+
+            if i == 1
+                fuerza_elastica =  k * (w(i+1,:) - 2*w(i,:) + w(end,:));
+            elseif i == size(w,1)
+                fuerza_elastica =  k * (w(1,:) - 2*w(i,:) + w(i-1,:));
+            else
+                fuerza_elastica =  k * (w(i+1,:) - 2*w(i,:) + w(i-1,:));
+            end
+
+            incremento = aprendizaje * (incremento + fuerza_elastica);
+
             w(i,:) = w(i,:) + incremento;
+
         end
-        
+
     end
     
     figure;
-    plot([w(:,1)' ], [w(:,2)' ], '.-b');
+    plot([w(:,1)' w(1,1)], [w(:,2)' w(1,2)], '.-b');
     hold on;
     plot(puntos(:,1), puntos(:,2), '.r');
     title(['sigma = ' num2str(sigma) ' k = ' num2str(k)]);
     saveas( gcf, fullfile('ej3',num2str(iteracion)), 'jpg' );
     iteracion = iteracion + 1;
-    
-    sigma = sigma*0.95;
+    sigma = sigma*0.9;
+    k = sigma/10;
 end
+
 
 if ~exist('data','dir')
     mkdir('data');
